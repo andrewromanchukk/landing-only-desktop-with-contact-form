51a9d62f6af4203176b22f791e23a25da145d218
andrewromanchukk <andrewromanchukk@gmail.com>
2020-05-31 12:24:47 +0300

    php is not good

--- mail.php
+++ mail.php
@@ -1,17 +1,69 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
 
-$check_msg='';
-foreach($_POST['connect'] as $value){
-    $check_msg .=$value.' ';
-}
+	header("Content-Type: text/html; charset=utf-8");
+	
+	if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"]) === "xmlhttprequest") {
+	
+		if(!isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"])) {
 
-$formcontent = "Від: $name \n Повідомлення: $message";
-$recipient = "readythomasgo@gmail.com";
-$subject = "Форма зворотнього звязку";
-$mailheader = "Відправник: $email \r \n";
-$info = mail($recipient, $subject, $formcontent, $mailheader, "Content-type:text/plain; Content-type:text/plain; charset = UTF-8\r\n") or die ("Ошибка!");
-echo "Thank You!" - "<a href='index.html' style='text-decoration:none;font-size:16px;color:#ff0099;'> Back to Main Page</a>";
+			die();
+
+		}
+	
+		function send_form($message) {
+	
+			$mail_to = "readythomasgo@gmail.com"; // Адрес, куда отправляем письма
+			$subject = "Письмо с обратной связи"; // Тема письма
+			$headers = "MIME-Version: 1.0\r\n";
+			$headers .= "Content-type: text/html; charset=utf-8\r\n";
+			$headers .= "From: Система уведомлений <no-reply@".$_SERVER['HTTP_HOST'].">\r\n";
+
+			mail($mail_to, $subject, $message, $headers);
+		
+		}
+
+		$name = strip_tags($_POST["name"]); // Имя
+		$email = strip_tags($_POST["email"]); // E-mail
+		$mess = strip_tags($_POST["message"]); // Сообщение
+
+		if(!preg_match("|^([a-z0-9_.-]{1,20})@([a-z0-9.-]{1,20}).([a-z]{2,4})|is", strtolower($email))) { // Если e-mail пустой или невалиден
+
+			echo "E-mail указан некорректно.";
+
+			die();
+
+		}
+
+		if($mess == "") { // Если сообщение пустое
+
+			echo "Не указан текст сообщения.";
+
+			die();
+
+		}
+
+		if($name == "") { // Если имя не указано
+
+			$name = "Не указано";
+
+		}
+
+		$message = <<<HTML
+
+			<b>Имя отправителя</b>: {$name}<br>
+			<b>E-mail</b>: {$email}<br><br>
+			<b>Текст письма</b>: {$mess}
+
+HTML;
+
+		send_form($message); // Если ранее описанных ошибок нет - отправляем сообщение
+		
+		echo "Сообщение успешно отправлено!";
+
+	} else {
+
+		die();
+
+	}
+
 ?>
--- script.js
+++ script.js
@@ -17,3 +17,35 @@ $(function(){
       */
     });
   });
+//   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+$(document).ready(function() {
+
+    $("#btn-submit").on("click", function() {
+    
+        var name = $("#name").val(); // Получаем имя
+        var email = $("#email").val(); // Получаем e-mail
+        var message = $("#message").val(); // Получаем сообщение
+        
+        $.ajax({
+        
+            url: "mail.php", // Куда отправляем данные (обработчик)
+            type: "post",
+
+            data: {
+                "name": name,
+                "email": email,
+                "message": message
+            },
+
+            success: function(data) {
+            
+                $(".result").html(data); // Выводим результат
+                
+            }
+            
+        });
+        
+    });
+    
+});
